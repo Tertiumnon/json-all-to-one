b13b2eaeadc0a143d65720a88a8887965086d3e9
Vit Tertiumnon <tertiumnon@gmail.com>
2019-06-22 11:09:03 +0300

    0.2.0

--- json-all-to-one.js
+++ json-all-to-one.js
@@ -1,75 +1,73 @@
 const fs = require('fs');
-const { pd } = require('pretty-data');
+const arg = require('arg');
 
-const dir = process.argv[2];
-const targetFile = process.argv[3];
+const args = arg({
+  // Types
+  '--source': String, // path to source filename
+  '--output': String, // output filename
+  '--name': String, // wrapper name
+  '--childrenOf': String, // get children of
+
+  // Aliases
+  '-s': '--source',
+  '-o': '--output',
+  '-n': '--name',
+  '-c': '--childrenOf',
+});
+
+args['--name'] = args['--name'] || 'data';
 
 let files = [];
-let count = 0;
+const newData = {
+  [args['--name']]: [],
+};
 
-const readDir = new Promise((resolve, reject) => fs.readdir(dir, (err, filenames) => {
+const getFilenames = new Promise((resolve, reject) => fs.readdir(args['--source'], (err, filenames) => {
+  console.time('json files merging');
   if (err) reject(err);
-  resolve(filenames);
+  resolve(files = filenames);
 }));
 
-const startWriting = () => new Promise((resolve, reject) => fs.writeFile(targetFile, '{\n"data":\n[', { flag: 'w' }, (err) => {
+const prettifyData = (data) => {
+  return JSON.stringify(data, null, 4);
+};
+
+const writeOutputFile = () => new Promise((resolve, reject) => fs.writeFile(args['--output'], prettifyData(newData), { flag: 'w' }, (err) => {
   if (err) reject(err);
   resolve(true);
 }));
 
-const writeFile = data => new Promise((resolve, reject) => fs.writeFile(targetFile, `\n${data},`, { flag: 'a+' }, (err) => {
+const addData = (data) => {
+  if (Array.isArray(data)) {
+    newData[args['--name']] = newData[args['--name']].concat(data);
+  } else {
+    newData[args['--name']].push(data);
+  }
+};
+
+const readFile = file => new Promise((resolve, reject) => fs.readFile(`${args['--source']}/${file}`, (err, data) => {
   if (err) reject(err);
-  count += 1;
-  process.stdout.write(`(1/2) files: ${count} / ${files.length}\r`);
-  resolve(true);
+  const parsedData = JSON.parse(data);
+  const fileData = args['--childrenOf'] ? parsedData[args['--childrenOf']] : parsedData;
+  resolve(addData(fileData));
 }));
 
-const readAndWriteFile = file => new Promise((resolve, reject) => fs.readFile(`${dir}/${file}`, (err, data) => {
-  if (err) reject(err);
-  resolve(writeFile(data));
-}));
-
-const writing = () => new Promise((resolve, reject) => {
-  const proms = files.map(readAndWriteFile);
+const readSourceFiles = () => new Promise((resolve, reject) => {
+  const proms = files.map(readFile);
   const promsRes = Promise.all(proms);
-  promsRes.then((res) => {
-    resolve(res);
-  }).catch((err) => {
-    reject(err);
-  });
+  promsRes
+    .then((res) => {
+      resolve(res);
+    })
+    .catch((err) => {
+      reject(err);
+    });
 });
 
-const endWriting = () => new Promise((resolve, reject) => {
-  fs.writeFile(targetFile, '\n]\n}', { flag: 'a' }, (err) => {
-    if (err) reject(err);
-    resolve(true);
-  });
-});
-
-const readTargetFile = () => new Promise((resolve, reject) => {
-  fs.readFile(targetFile, 'utf8', (err, data) => {
-    if (err) reject(err);
-    resolve(data);
-  });
-});
-
-const prettifyTargetFile = data => new Promise((resolve, reject) => {
-  fs.writeFile(targetFile, pd.json(data.replace('},\n]', '}\n]')), (err) => {
-    if (err) reject(err);
-    resolve(true);
-  });
-});
-
-readDir
-  .then((res) => {
-    files = res;
-    return startWriting(res);
-  })
-  .then(() => writing())
-  .then(() => endWriting())
-  .then(() => readTargetFile())
-  .then(data => prettifyTargetFile(data))
-  .then(() => process.stdout.write('\n(2/2) done'))
+getFilenames
+  .then(() => readSourceFiles())
+  .then(() => writeOutputFile())
+  .then(() => console.timeEnd('json files merging'))
   .catch((err) => {
     throw err;
   });
